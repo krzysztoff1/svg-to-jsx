d6d1a519ed74262a9becbd7cd603638bf5b4ca86
Krzysztof <halidu13@gmail.com>
2023-09-17 16:07:09 +0200

    feat: add copy button to result

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,12 +1,33 @@
 import { useAtomValue } from "jotai";
 import { outputAtom } from "@/atoms/editor";
 import CodeEditor from "@uiw/react-textarea-code-editor";
+import { Button } from "./ui/button";
+import { useEffect, useState } from "react";
+import { Check, Copy } from "lucide-react";
 
 export function Result() {
   const output = useAtomValue(outputAtom);
 
+  const [isCopied, setIsCopied] = useState(false);
+
   return (
-    <section className="w-full">
+    <section className="w-full relative">
+      <Button
+        variant={"outline"}
+        className="absolute top-0 right-0 m-2 z-10 p-1 shadow-md aspect-square"
+        onClick={() => {
+          navigator.clipboard.writeText(output);
+          setIsCopied(true);
+          setTimeout(() => setIsCopied(false), 2000);
+        }}
+      >
+        {isCopied ? (
+          <Check className="w-3 h-3" />
+        ) : (
+          <Copy className="w-3 h-3" />
+        )}
+      </Button>
+
       <CodeEditor
         value={output}
         language="js"
